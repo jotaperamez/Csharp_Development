23b103b7d642f9a2b444427436e93082a3b007b7
Juan Pedro Ramos Gómez <jrg065@inlumine.ual.es>
2015-02-27 22:41:40 +0100

    Ejercicio01

--- Clase01/Ejercicio01/Program.cs
+++ Clase01/Ejercicio01/Program.cs
@@ -10,6 +10,19 @@ namespace Ejercicio01
     {
         static void Main(string[] args)
         {
+            /*
+             * Enunciado: Nos vamos de compras al super y compramos
+             * 2 kg de manzanas y 1.5 kg de peras.
+             * El precio de ambos productos es de 0.99 €/kg
+             * ¿Cuanto cuesta la compra total? 
+             */
+
+            double kgManzana = 2;
+            double kgPeras = 1.5;
+            double proporcionEuroKg = 0.99;
+            Console.WriteLine("La compra cuesta: " + (kgManzana * proporcionEuroKg + kgPeras * proporcionEuroKg));
+            Console.ReadKey();
+
         }
     }
 }
